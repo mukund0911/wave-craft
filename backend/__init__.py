77d8c14eeec8ea28c8bb3976d03cad30205c2449
Mukund Telukunta <mt3qb@umsystem.edu>
2025-01-06 16:56:47 -0600

    attemp 9 to fix https issue

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,12 +4,12 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, 
-         origins=['https://wave-crafter.com', 'http://localhost:3000'],
-         methods=['GET', 'POST', 'OPTIONS'],
-         allow_headers=['Content-Type'],
-         supports_credentials=True,
-         expose_headers=['Content-Type'])
+    CORS(app) 
+     #     origins=['https://wave-crafter.com', 'http://localhost:3000'],
+     #     methods=['GET', 'POST', 'OPTIONS'],
+     #     allow_headers=['Content-Type'],
+     #     supports_credentials=True,
+     #     expose_headers=['Content-Type'])
 
     from .routes import main
     app.register_blueprint(main)
